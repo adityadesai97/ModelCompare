f228aefdf13f7c600cdfb5aea9bdb78ec5c8ff9d
Nandini Krishnaswamy <nandinik@berkeley.edu>
2020-12-15 05:56:16 +0000

    xlnet works

--- .ipynb_checkpoints/model-checkpoint.py
+++ .ipynb_checkpoints/model-checkpoint.py
@@ -10,13 +10,15 @@ from tensorflow.keras.layers import Input, Activation, Embedding, Bidirectional,
 class Model:
 
 
-	MODEL_MAP = {'bert': ('bert-base-uncased', ''), 'distilbert': ('distilbert-base-uncased', ''), 'roberta': ('roberta-base', ''), 'xlnet': ('xlnet-base-uncased', '')}
+	MODEL_MAP = {'bert': ('bert-base-uncased', ''), 'distilbert': ('distilbert-base-uncased', ''), 'roberta': ('roberta-base', ''), 'xlnet': ('xlnet-base-cased', '')}
 
 	MODEL_INPUTS = {'bert': ['input_ids', 'token_type_ids', 'attention_mask'], 'distilbert': ['input_ids', 'token_type_ids', 'attention_mask'], 
-					'roberta': ['input_ids', 'attention_mask'], 'xlnet': []}
+					'roberta': ['input_ids', 'attention_mask'], 'xlnet': ['input_ids', 'token_type_ids', 'attention_mask']}
 
 	BATCH_SIZE = 32
 	MAX_SEQ_LEN = 128
+	LEARNING_RATE = 3e-5
+    
 
 	ALPHA = 0.1
 
